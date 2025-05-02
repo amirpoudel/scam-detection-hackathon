4a99acd57d1a6f400be1450dfd41479e8a73d38b
bishal <bisugaihre9846@gmail.com>
2025-05-03 00:57:50 +0545

    setup

---
+++ scam-detection-frontend/src/App.js
@@ -0,0 +1,15 @@
+// import "./App.css";
+import "./index.js";
+import React from "react";
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <p className="text-red-500 text-xl">hello</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ scam-detection-frontend/tailwind.conifg.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: [
+    "./public/index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/*.{js,jsx,ts,tsx}",
+    "../node_modules/**",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require("tailwindcss"),
+    require("autoprefixer"),
+    // require("@tailwindcss/typography"),
+    // require("@tailwindcss/forms"),
+    // require("@tailwindcss/aspect-ratio"),
+    // require("tailwindcss-animatecss"),
+  ],
+};
