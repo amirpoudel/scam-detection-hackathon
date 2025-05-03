a58a4450b1d6a76669e7dd7e3c1caba1735ad8df
ArbitChaudhary <chaudharyarbit45s@gmail.com>
2025-05-03 06:22:06 -0400

    minor fixing

--- frontend/src/pages/_components/TextInput.jsx
+++ frontend/src/pages/_components/TextInput.jsx
@@ -11,7 +11,7 @@ export default function TextInput({ inputText, setInputText }) {
         id="message"
         rows="4"
         className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none placeholder:text-gray-500 text-black"
-        placeholder="Paste suspicious WhatsApp message here..."
+        placeholder="Paste suspicious message here..."
         value={inputText}
         onChange={(e) => setInputText(e.target.value)}
       ></textarea>
